b66782a5241d3fcd8a7850cb980f5b4e91c9786f
thanhcdak <thanhcdak@gmail.com>
2018-06-01 21:06:43 +0700

    add-project

---
+++ app/src/main/java/thanh/com/quanly/HienThiSinhVien.java
@@ -0,0 +1,14 @@
+package thanh.com.quanly;
+
+/**
+ * @author Phạm Công Thành
+ * @version 1.0.0
+ * @description
+ * @created ${Date}
+ * @updated ${Date}
+ * @modified by
+ * @updated on ${Date}
+ * @since 1.0
+ */
+public class HienThiSinhVien {
+}
